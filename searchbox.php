4efc5422d89486a7b7fbf4312b9b8c95eb90c305
Theslimecubes <Jeerdhuyzen.je@gmail.com>
2024-11-26 10:48:26 +0100

    table_unfinished

---
+++ searchbox.php
@@ -0,0 +1,24 @@
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+        
+        
+        <form actions="">
+        <div class="input-group mb-3">
+
+        <input type="text" class="form-control" value=""<?php if(isset($_GET['search'])){echo $_GET['search'];} ?> name="search" placeholder="search here. . .">
+ 
+        <button type="submit" class="btn btn-primary">Zoeken</button>
+   
+         </div>
+         </form>
+      </div>
+
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+            <h4 class="text-center">Table search test</h4>
+            <div class="card-body">
+   </div>
+</div>
+
