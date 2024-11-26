08e7de7e019fdb7e9eb397202647691698605930
Theslimecubes <Jeerdhuyzen.je@gmail.com>
2024-11-26 11:51:10 +0100

    searchbox updated

--- searchbox.php
+++ searchbox.php
@@ -1,3 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Producten overzicht</title>
+</head>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css">
+<link rel="stylesheet" href="producten.css">
+<body>
+
+   
+    
+    
+
+
+
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -17,8 +34,66 @@
       <div class="col-md-12">
         <div class="card">
           <div class="card-header">
-            <h4 class="text-center">Table search test</h4>
+            <h4 class="text-center"></h4>
             <div class="card-body">
-   </div>
+
+            <div class="knop-container">
+        <button class="toevoegen-knop">
+            <span class="icon">+</span> Toevoegen
+        </button>
+    </div>
+    
+    <table>
+        <thead>
+            <tr>
+                <th>Ean</th>
+                <th>Naam</th>
+                <th>Categorie ID</th>
+                <th>Houdsbaarheid datum</th>
+                <th>Aantal</th>
+            </tr>
+        </thead>
+        <tbody id="">
+          <?php 
+          
+          if(isset($_GET['search']))
+          {
+            $connection = mysqli_connect("localhost", "root", "", "voedselbankdb");
+            $filtervalue = $_GET['search'];
+            $filterdata = "SELECT * FROM producten WHERE CONCAT(id, naam, beschrijving, houdbaarheidsdatum, categorie_id) LIKE '%$filtervalue%'";
+            $filterdata_run = mysqli_query($connection, $filterdata);
+
+            if(mysqli_num_rows($filterdata_run) > 0)
+            {
+foreach($filterdata_run as $row)
+{
+  ?>
+  <tr>
+    <td><?php echo $row['id']; ?></td>
+    <td><?php echo $row['naam']; ?></td>
+    <td><?php echo $row['beschrijving']; ?></td>
+    <td><?php echo $row['houdbaarheidsdatum']; ?></td>
+    <td><?php echo $row['categorie_id']; ?></td>
+  </tr>
+  <?php
+}
+            }
+            else
+            {
+?>
+<tr>
+  <td colspan="4">Geen data gevonden...</td>
+</tr>
+<?php
+             }
+          }
+
+          ?>
+            <!-- Event data wordt hier geladen -->
+        </tbody>
+    </table>
+</div>
 </div>
 
+        </body>
+</html>
