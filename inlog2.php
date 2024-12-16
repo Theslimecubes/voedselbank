302cf2446008adcdac29cd501327774fdd13fdf8
AlexisCunha <acunha@roc-dev.com>
2024-12-16 10:28:58 +0100

    inlog head gefixt

--- inlog2.php
+++ inlog2.php
@@ -1,19 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welkom</title>
-    <link rel="stylesheet" href="inlog.css">
-  </head>
-  <head>
     <link rel="stylesheet" type="text/css" href="inlog.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welkom</title>
     <link rel="stylesheet" href="inlog.css">
   </head>
-
   <body>
     <div class="login-container">
       <div class="logo">
