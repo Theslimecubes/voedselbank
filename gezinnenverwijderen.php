ccd4a33c62a3de2323553044b398fb582384e15e
Mae <Lisa.wijnands004@gmail.com>
2024-12-17 22:13:16 +0100

    gezin verwijder changes

--- gezinnenverwijderen.php
+++ gezinnenverwijderen.php
@@ -1,69 +1,20 @@
 <?php
 
-$gebruikersnaam = $_GET['gebruikersnaam'];
 $connection = mysqli_connect("localhost", "root", "", "voedselbankdb");
 
 if (!$connection) {
     die("Verbinding met database mislukt: " . mysqli_connect_error());
 }
 
-$sql = "DELETE FROM gebruikers WHERE gebruikersnaam = $gebruikersnaam"; 
+$gebruikersnaam = $_GET['gebruikersnaam'];
+$sql = "DELETE FROM gebruikers WHERE gebruikersnaam = '$gebruikersnaam'";
+echo $sql;
 
 if (mysqli_query($connection, $sql)) {
-    mysqli_close($connection);
-    header('Location: gezinnen.php');
-    exit;
+  echo "Record deleted successfully";
+  header("Location: gezinnen.php");
 } else {
-    echo "Error deleting record";
+  echo "Error deleting record: " . mysqli_error($connection);
 }
+
 ?>
-
-
-
-
-
-
-
-
-<!-- <?php
-
-$connection = mysqli_connect("localhost", "root", "", "voedselbankdb");
-$tableName="gebruikers";
-
-if (!$connection) {
-    die("Verbinding met database mislukt: " . mysqli_connect_error());
-}
-
-if(isset($_GET['delete']))
-{
-  $gebruikersnaam= valgebruikersnaamate($_GET['delete']);
-  $condition =['gebruikersnaam'=>$gebruikersnaam];
-  $deleteMsg=delete_data($connection, $tableName, $condition);
-  header("location:form.php");
-}
-function delete_data($connection, $tableName, $condition){
-    $conditionData='';
-    $i=0;
-    foreach($condition as $index => $data){
-        $and = ($i > 0)?' AND ':'';
-         $conditionData .= $and.$index." = "."'".$data."'";
-         $i++;
-    }
-  $query= "DELETE FROM ".$tableName." WHERE ".$conditionData;
-  $result= $db->query($query);
-  if($result){
-    $msg="data was deleted successfully";
-  }else{
-    $msg= $db->error;
-  }
-  return $msg;
-}
-function valgebruikersnaamate($value) {
-$value = trim($value);
-$value = stripslashes($value);
-$value = htmlspecialchars($value);
-return $value;
-}
-
-mysqli_close($connection);
-?> -->
