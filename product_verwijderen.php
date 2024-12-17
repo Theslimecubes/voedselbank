fdf478f372cf74349c78e02de402d4c3452825c3
Theslimecubes <Jeerdhuyzen.je@gmail.com>
2024-12-17 21:53:00 +0100

    producten update

---
+++ product_verwijderen.php
@@ -0,0 +1,20 @@
+<?php
+
+$connection = mysqli_connect("localhost", "root", "", "voedselbankdb");
+
+if (!$connection) {
+    die("Verbinding met database mislukt: " . mysqli_connect_error());
+}
+
+$id = $_GET['id'];
+$sql = "DELETE FROM producten WHERE id= '$id'";
+echo $sql;
+
+if (mysqli_query($connection, $sql)) {
+  echo "Record deleted successfully";
+  header("Location: producten.php");
+} else {
+  echo "Error deleting record: " . mysqli_error($connection);
+}
+
+?>
